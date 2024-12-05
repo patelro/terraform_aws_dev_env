0e14ff8a23df5cfc03f884d8fd294f3a7b2c9740
patelro <55508685+patelro@users.noreply.github.com>
2024-12-05 12:17:22 -0500

    update egress cidr_ipv4

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_vpc_security_group_ingress_rule" "mtc_sg_ingress" {
 
 resource "aws_vpc_security_group_egress_rule" "mtc_sg_egress" {
   security_group_id = aws_security_group.mtc_sg.id
-  cidr_ipv4         = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
